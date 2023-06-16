a5cead5cafcbe02a984b3c7e05bcf112698b63d5
ilko-iliev-ovo <ilko.iliev@ovo.com>
2023-06-16 21:42:58 +0300

    Added compiler optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,26 +6,28 @@ import "./scripts/deployment/customDeploy";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+          enabled: true,
+          runs: 1000
+      }
+    }
+  } ,
   networks: {
     sepolia: {
       url: secrets.INFURA_SEPOLIA_URL,
       accounts: [secrets.PRIVATE_KEY],
       allowUnlimitedContractSize: true,
-      gasLimit: 5000000,
-      gas: 5000000
     },
     goerli: {
       url: secrets.INFURA_GOERLI_URL,
       accounts: [secrets.PRIVATE_KEY],
       allowUnlimitedContractSize: true,
-      gasLimit: 5000000,
-      gas: 5000000
     },
     development: {
       url: "http://127.0.0.1:8545",
-      gasLimit: 5000000,
-      gas: 5000000
     },
   },
   etherscan: {
