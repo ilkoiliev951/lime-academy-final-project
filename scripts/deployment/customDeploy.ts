82f6cb61f2a57e1f2fa6b6971f1a0879e2161ff1
ilko-iliev-ovo <ilko.iliev@ovo.com>
2023-06-19 02:48:41 +0300

    Refactored deployment task

--- scripts/deployment/customDeploy.ts
+++ scripts/deployment/customDeploy.ts
@@ -1,20 +1,32 @@
+import {Contract} from "ethers";
+import {subtask, task} from "hardhat/config";
+import {ethers} from "hardhat";
+
 require("hardhat/config");
-import {task, subtask} from "hardhat/config";
 
 task("deploy-custom", "Deploys a custom contract")
-    .setAction(async (taskArgs, {run}) => {
+    .setAction(async (taskArgs, {run, network}) => {
         const {ethers} = require("hardhat");
         const bridgeContractFactory = await ethers.getContractFactory("EVMBridge");
         const bridge = await bridgeContractFactory.deploy();
         await bridge.deployed();
         console.log(`The Bridge Contract was deployed to ${bridge.address}`);
 
-        let subtaskArgs = {
-            address: bridge.address,
-            hash: bridge.deployTransaction.hash,
-            senderAddress: bridge.deployTransaction.from
+        await run("print-custom-deployment-info", buildSubtaskArgs(bridge));
+
+        if (network.name === 'sepolia' || network.name === 'development') {
+            const genericERC20ContractFactory = await ethers.getContractFactory("GenericERC20");
+            const genericERC20 = await genericERC20ContractFactory.deploy();
+            await genericERC20 .deployed();
+            await run("print-custom-deployment-info", buildSubtaskArgs(genericERC20));
         }
-        await run("print-custom-deployment-info", subtaskArgs);
+
+        if (network.name === 'goerli' || network.name === 'development') {
+            const wrappedERC20ContractFactory = await ethers.getContractFactory("WrappedERC20");
+            const wrappedERC20 = await wrappedERC20ContractFactory.deploy();
+            await wrappedERC20 .deployed();
+            await run("print-custom-deployment-info", buildSubtaskArgs(wrappedERC20));
+        }
     });
 
 subtask("print-custom-deployment-info", "Prints information about the contract after successful deployment.")
@@ -25,3 +37,11 @@ subtask("print-custom-deployment-info", "Prints information about the contract a
         await console.log(`- Transaction Hash: ${taskArgs.hash}`);
         await console.log(`- Deployer Address: ${taskArgs.senderAddress}`);
     });
+
+async function buildSubtaskArgs (contract: Contract) {
+    return {
+        address: contract.address,
+        hash: contract.deployTransaction.hash,
+        senderAddress: contract.deployTransaction.from
+    };
+}
