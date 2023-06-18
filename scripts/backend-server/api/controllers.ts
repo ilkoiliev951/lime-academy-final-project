93be2ee83023cadd5907e406d85a5e5ccb1b9b27
ilko-iliev-ovo <ilko.iliev@ovo.com>
2023-06-18 14:29:22 +0300

    Redid package structure

--- scripts/backend-server/api/controllers.ts
+++ scripts/backend-server/api/controllers.ts
@@ -6,11 +6,19 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.post('/api/bridge/lock', (req, res) => {
+app.post('/api/bridge/fetch-locked', (req, res) => {
     res.send('Express + TypeScript Server');
 });
 
-app.post('/api/bridge/claim', (req, res) => {
+app.post('/api/bridge/fetch-burnt', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.post('/api/bridge/fetch-bridged-by-wallet', (req, res) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.post('/api/bridge/fetch-all-bridged', (req, res) => {
     res.send('Express + TypeScript Server');
 });
 
