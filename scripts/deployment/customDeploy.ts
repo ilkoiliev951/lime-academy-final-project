94f55d74a0a41f17fd98ab41e3e970a1c4768456
ilko-iliev-ovo <ilko.iliev@ovo.com>
2023-06-19 03:53:27 +0300

    Refactored deployment task

--- scripts/deployment/customDeploy.ts
+++ scripts/deployment/customDeploy.ts
@@ -12,34 +12,35 @@ task("deploy-custom", "Deploys a custom contract")
         await bridge.deployed();
         console.log(`The Bridge Contract was deployed to ${bridge.address}`);
 
-        await run("print-custom-deployment-info", buildSubtaskArgs(bridge));
+        await run("print-custom-deployment-info", buildSubtaskArgs(bridge, "EVMBridge"));
 
         if (network.name === 'sepolia' || network.name === 'development') {
             const genericERC20ContractFactory = await ethers.getContractFactory("GenericERC20");
             const genericERC20 = await genericERC20ContractFactory.deploy();
             await genericERC20 .deployed();
-            await run("print-custom-deployment-info", buildSubtaskArgs(genericERC20));
+            await run("print-custom-deployment-info", buildSubtaskArgs(genericERC20, "GenericERC20"));
         }
 
         if (network.name === 'goerli' || network.name === 'development') {
             const wrappedERC20ContractFactory = await ethers.getContractFactory("WrappedERC20");
             const wrappedERC20 = await wrappedERC20ContractFactory.deploy();
             await wrappedERC20 .deployed();
-            await run("print-custom-deployment-info", buildSubtaskArgs(wrappedERC20));
+            await run("print-custom-deployment-info", buildSubtaskArgs(wrappedERC20, "WrappedERC20"));
         }
     });
 
 subtask("print-custom-deployment-info", "Prints information about the contract after successful deployment.")
     .setAction(async (taskArgs) => {
         await console.log("Deployment Information:");
-        await console.log(`- Contract Name: Library`);
+        await console.log(`- Contract Name: ${taskArgs.name}`);
         await console.log(`- Contract Address: ${taskArgs.address}`);
         await console.log(`- Transaction Hash: ${taskArgs.hash}`);
         await console.log(`- Deployer Address: ${taskArgs.senderAddress}`);
     });
 
-async function buildSubtaskArgs (contract: Contract) {
+async function buildSubtaskArgs (contract: Contract, contractName: string) {
     return {
+        name: contractName,
         address: contract.address,
         hash: contract.deployTransaction.hash,
         senderAddress: contract.deployTransaction.from
